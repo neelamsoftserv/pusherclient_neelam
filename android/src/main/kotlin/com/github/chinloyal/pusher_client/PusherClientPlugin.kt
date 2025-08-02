a711c1fc92b2c0641ceb7a4a73d35e7d31e9b3b8
neelamsoftserv <122078222+neelamsoftserv@users.noreply.github.com>
2025-08-02 16:08:59 +0530

    PusherClientPlugin.kt changes

--- android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
+++ android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
@@ -1,14 +1,14 @@
-package com.github.chinloyal.pusher_client
-
-import androidx.annotation.NonNull
-import com.github.chinloyal.pusher_client.pusher.PusherService
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-//import io.flutter.plugin.common.PluginRegistry.Registrar
+//package com.github.chinloyal.pusher_client
+//
+//import androidx.annotation.NonNull
+//import com.github.chinloyal.pusher_client.pusher.PusherService
+//
+//import io.flutter.embedding.engine.plugins.FlutterPlugin
+//import io.flutter.plugin.common.MethodCall
+//import io.flutter.plugin.common.MethodChannel
+//import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+//import io.flutter.plugin.common.MethodChannel.Result
+////import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** PusherClientPlugin
 class PusherClientPlugin: FlutterPlugin {
@@ -21,20 +21,33 @@ class PusherClientPlugin: FlutterPlugin {
 //    channel.setMethodCallHandler(null)
   }
 }*/
+package com.github.chinloyal.pusher_client
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** PusherClientPlugin */
 class PusherClientPlugin: FlutterPlugin, MethodCallHandler {
+
   private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "pusher_client")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    // TODO: Handle method calls from Dart
+    result.notImplemented()
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    // Handle method calls here
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
+
 

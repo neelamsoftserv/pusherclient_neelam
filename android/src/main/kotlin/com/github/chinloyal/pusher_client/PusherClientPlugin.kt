54f29817c00df52ab56831d0633d4ed629411dd5
neelamsoftserv <122078222+neelamsoftserv@users.noreply.github.com>
2025-08-02 19:06:48 +0530

    PusherClientPlugin.kt changes

--- android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
+++ android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
@@ -1,32 +1,23 @@
 package com.github.chinloyal.pusher_client
 
 import androidx.annotation.NonNull
+import com.github.chinloyal.pusher_client.pusher.PusherService
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** PusherClientPlugin */
-class PusherClientPlugin: FlutterPlugin, MethodCallHandler {
-
-  private lateinit var channel: MethodChannel
+class PusherClientPlugin: FlutterPlugin {
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.github.chinloyal/pusher_client")
-    val streamChannel = EventChannel(flutterPluginBinding.binaryMessenger, "com.github.chinloyal/pusher_client_stream")
-    streamChannel.setStreamHandler(this)
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    // TODO: Handle method calls from Dart
-    result.notImplemented()
+    PusherService().register(flutterPluginBinding.binaryMessenger);
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+//    channel.setMethodCallHandler(null)
   }
 }
-
-

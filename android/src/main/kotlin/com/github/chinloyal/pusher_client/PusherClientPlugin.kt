585d6ef9c8836341511e9134ef97fcaf034ea6e3
neelamsoftserv <122078222+neelamsoftserv@users.noreply.github.com>
2025-08-02 15:13:48 +0530

    changes in pusherClientPLugin.kt

--- android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
+++ android/src/main/kotlin/com/github/chinloyal/pusher_client/PusherClientPlugin.kt
@@ -10,7 +10,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** PusherClientPlugin */
+/** PusherClientPlugin
 class PusherClientPlugin: FlutterPlugin {
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -20,4 +20,21 @@ class PusherClientPlugin: FlutterPlugin {
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
 //    channel.setMethodCallHandler(null)
   }
+}*/
+class PusherClientPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel: MethodChannel
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "pusher_client")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    // Handle method calls here
+  }
 }
+
